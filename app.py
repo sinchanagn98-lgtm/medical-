0cc2b2b9c0408033cb5e4e9b429785600bef800d
sinchana <sinchanagn98@gmail.com>
2026-06-06 09:59:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,46 @@
-
 import streamlit as st
 
+# Page Config
 st.set_page_config(
     page_title="Medical Device Analytics",
     page_icon="🏥",
     layout="wide"
 )
 
-st.title("🏥 Medical Device Failure Analytics Dashboard")
+# Load CSS
+with open("assets/style.css") as f:
+    st.markdown(
+        f"<style>{f.read()}</style>",
+        unsafe_allow_html=True
+    )
 
+# Banner
 st.markdown("""
-Welcome to the advanced healthcare analytics dashboard.
+<div style="
+    background: linear-gradient(90deg,#0f172a,#1e40af);
+    padding: 40px;
+    border-radius: 20px;
+    text-align: center;
+    color: white;
+">
 
-Use the sidebar to navigate between pages.
-""")
+<h1>🏥 Medical Device Failure Analytics</h1>
 
+<h3>Advanced Healthcare Monitoring Dashboard</h3>
+
+<p>
+📊 Deep Analytics |
+🌍 Global Insights |
+⚠️ Failure Prediction |
+🧠 Smart Monitoring
+</p>
+
+</div>
+""", unsafe_allow_html=True)
+
+st.write("")
+
+# KPI Cards
 col1, col2, col3, col4 = st.columns(4)
 
 col1.metric("Total Records", "4000+")
@@ -22,6 +48,6 @@ col2.metric("Countries", "20+")
 col3.metric("Manufacturers", "15+")
 col4.metric("Analytics", "Deep Insights")
 
-st.image("assets/banner.png", use_container_width=True)
+st.divider()
 
-st.success("Project Loaded Successfully ✅")
+st.success("Dashboard Loaded Successfully ✅")
